3ea5f2949a6679649453b2c5d9456f6a9aa8e510
Matej Refka <matejrefka@googlemail.com>
2025-04-27 22:19:23 +0100

    DI container and dependency tests

---
+++ tests/PulseInject.Tests/DependencyContainerTests.cs
@@ -0,0 +1,72 @@
+﻿using LambdaPulse.DI;
+
+namespace LambdaPulse.Tests.DI
+{
+    public class DependencyContainerTests
+    {
+        [Fact]
+        public void AddSingleton_AddsSingletonDependency()
+        {
+            //arrange
+            var container = new DependencyContainer();
+
+            //act
+            container.AddSingleton<string>();
+            var dependency = container.GetDependency(typeof(string));
+
+            //assert
+            Assert.NotNull(dependency);
+            Assert.Equal(typeof(string), dependency.Type);
+            Assert.Equal(DependencyLifetime.Singleton, dependency.Lifetime);
+        }
+
+        [Fact]
+        public void AddScoped_AddsScopedDependency()
+        {
+            //arrange
+            var container = new DependencyContainer();
+
+            //act
+            container.AddScoped<string>();
+            var dependency = container.GetDependency(typeof(string));
+
+            //assert
+            Assert.NotNull(dependency);
+            Assert.Equal(typeof(string), dependency.Type);
+            Assert.Equal(DependencyLifetime.Scoped, dependency.Lifetime);
+        }
+
+        [Fact]
+        public void AddTransient_AddsTransientDependency()
+        {
+            //arrange
+            var container = new DependencyContainer();
+
+            //act
+            container.AddTransient<string>();
+            var dependency = container.GetDependency(typeof(string));
+
+            //assert
+            Assert.NotNull(dependency);
+            Assert.Equal(typeof(string), dependency.Type);
+            Assert.Equal(DependencyLifetime.Transient, dependency.Lifetime);
+        }
+
+        [Fact]
+        public void GetDependency_ReturnsDependencyByType()
+        {
+            //arrange
+            var container = new DependencyContainer();
+            container.AddSingleton<string>();
+            container.AddTransient<int>();
+
+            //act
+            var dependency = container.GetDependency(typeof(int));
+
+            //assert
+            Assert.NotNull(dependency);
+            Assert.Equal(typeof(int), dependency.Type);
+            Assert.Equal(DependencyLifetime.Transient, dependency.Lifetime);
+        }
+    }
+}
---
+++ tests/PulseInject.Tests/DependencyTests.cs
@@ -0,0 +1,51 @@
+﻿using LambdaPulse.DI;
+
+namespace LambdaPulse.Tests.DI
+{
+    public class DependencyTests
+    {
+        [Fact]
+        public void Constructor_SetsProperties()
+        {
+            //arrange
+            var expectedType = typeof(string);
+            var expectedLifetime = DependencyLifetime.Singleton;
+
+            //act
+            var dependency = new Dependency(expectedType, expectedLifetime);
+
+            //assert
+            Assert.Equal(expectedType, dependency.Type);
+            Assert.Equal(expectedLifetime, dependency.Lifetime);
+            Assert.Null(dependency.Instance);
+        }
+
+        [Fact]
+        public void CacheInstance_SetInstanceWhenNull()
+        {
+            //arrange
+            var dependency = new Dependency(typeof(string), DependencyLifetime.Transient);
+            var expectedInstance = "some string instance";
+
+            //act
+            dependency.CacheInstance(expectedInstance);
+
+            //assert
+            Assert.Equal(expectedInstance, dependency.Instance);
+        }
+
+        [Fact]
+        public void CacheInstance_SetInstanceWhenNotNull()
+        {
+            //arrange
+            var dependency = new Dependency(typeof(string), DependencyLifetime.Singleton);
+            dependency.CacheInstance("first instance");
+
+            //act & assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dependency.CacheInstance("second instance");
+            });
+        }
+    }
+}
